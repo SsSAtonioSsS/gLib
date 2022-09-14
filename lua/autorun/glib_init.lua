aa9f36cfd7c48f140e74c690132fcaa09bf1ba9d
SsSAtonioSsS <207anton207@gmail.com>
2022-09-14 15:04:02 +0300

    new ver

--- lua/autorun/glib_init.lua
+++ lua/autorun/glib_init.lua
@@ -1,6 +1,6 @@
 if not SERVER then return end
 gLib = gLib or {}
 gLib.SQL = gLib.SQL or {}
-gLib.version = '0.0.4'
+gLib.version = '0.0.4d'
 include('db/sv_connection.lua')
 include('d/sv_base.lua')
