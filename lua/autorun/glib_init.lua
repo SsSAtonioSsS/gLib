4cc23795e94b144cbfa0b8e141600b5315bc3b8f
SsSAtonioSsS <207anton207@gmail.com>
2022-09-18 09:48:54 +0300

    new ver

--- lua/autorun/glib_init.lua
+++ lua/autorun/glib_init.lua
@@ -1,6 +1,6 @@
 if not SERVER then return end
 gLib = gLib or {}
-gLib.version = '0.0.5d'
+gLib.version = '0.0.6d'
 gLib.branch = 'https://raw.githubusercontent.com/SsSAtonioSsS/yolo/public'
 include('db/sv_connection.lua')
 include('d/sv_base.lua')
